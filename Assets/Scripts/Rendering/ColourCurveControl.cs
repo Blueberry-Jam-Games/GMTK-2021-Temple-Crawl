f298ca8fea6175c3aeb9c463d196e36a78cd9783
Kaiden Richardson <kaidenr@shaw.ca>
2021-06-11 20:14:54 -0700

    Made saturation changing work properly and left it open for expansion later.

--- Assets/Scripts/Rendering/ColourCurveControl.cs
+++ Assets/Scripts/Rendering/ColourCurveControl.cs
@@ -6,22 +6,47 @@ using UnityEngine.Rendering.Universal;
 
 public class ColourCurveControl : MonoBehaviour
 {
-    Volume camVolume;
+    private Volume camVolume;
+    private ColorCurves colourControl;
+
+    private TextureCurve localCurve;
+
+    private Keyframe[] keyframes = null;
+
+    [SerializeField] Pair[] keyframeInput;
+
     // Start is called before the first frame update
     void Start()
     {
+        keyframes = new Keyframe[keyframeInput.Length];
+        for(int i = 0, count = keyframeInput.Length; i < count; i++)
+        {
+            keyframes[i] = new Keyframe(keyframeInput[i].time, keyframeInput[i].value);
+        }
         camVolume = GetComponent<Volume>();
         VolumeProfile profile = camVolume.sharedProfile;
-        if(profile.TryGet<ColorCurves>(out ColorCurves colourcontrol))
+        if(profile.TryGet<ColorCurves>(out ColorCurves cols))
         {
             Debug.Log("colour control found");
-            colourcontrol.hueVsSat = new TextureCurveParameter(new TextureCurve(new[] { new Keyframe(0f, 0f, 1f, 1f), new Keyframe(1f, 1f, 1f, 1f) }, 0f, false, new Vector2(0f, 1f)));
+            colourControl = cols;
+            colourControl.hueVsSat.overrideState = true;
         }
+
+        localCurve = new TextureCurve(new AnimationCurve(keyframes), 0f, true, new Vector2(0, 1));
+        colourControl.hueVsSat.Override(localCurve);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateKeyframe(int index, float newValue)
     {
-        
+        keyframes[index].value = newValue;
+        localCurve.MoveKey(index, keyframes[index]);
+        colourControl.hueVsSat.Override(localCurve);
+    }
+
+    [System.Serializable]
+    private struct Pair
+    {
+        public float time;
+        public float value;
     }
 }
